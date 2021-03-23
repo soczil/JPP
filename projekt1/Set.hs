3c15b08b5669a1777fdb29277c4060ce1035705a
soczil <ksoczewica@gmail.com>
2021-03-24 00:13:49 +0100

    projekt1 WIP

--- projekt1/Set.hs
+++ projekt1/Set.hs
@@ -34,15 +34,18 @@ toList (Singleton x) = [x]
 toList (Union s1 s2) = toList s1 ++ toList s2
 
 toAscList :: Ord a => Set a -> [a]
-toAscList = L.sort . toList 
+toAscList = removeDuplicates . L.sort . toList
+    where 
+        removeDuplicates l = foldr (\x acc -> if x `elem` acc then acc else x : acc) [] l
 
 elems :: Set a -> [a]
 elems = toList
 
 union :: Set a -> Set a -> Set a
-union s Empty = s
-union Empty s = s
-union s1 s2 = Union s1 s2
+-- union s Empty = s
+-- union Empty s = s
+-- union s1 s2 = Union s1 s2
+union = Union
 
 insert :: a -> Set a -> Set a
 insert x Empty = Singleton x
@@ -54,8 +57,6 @@ instance Ord a => Eq (Set a) where
     s1 == s2 = toAscList s1 == toAscList s2
 
 instance Semigroup (Set a) where
-    s <> Empty = s
-    Empty <> s = s
     s1 <> s2 = s1 `union` s2
     
 instance Monoid (Set a) where
@@ -66,4 +67,7 @@ instance Show a => Show (Set a) where
     show (Singleton x) = show x
     show (Union s1 s2) = show (s1, s2)
 
--- instance Functor Set where
+instance Functor Set where
+    fmap f Empty = Empty
+    fmap f (Singleton x) = Singleton (f x)
+    fmap f (Union s1 s2) = Union (fmap f s1) (fmap f s2)

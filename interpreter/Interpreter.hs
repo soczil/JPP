a7712d47a5e55e36c875bfc644de67a66b13d59f
soczil <ksoczewica@gmail.com>
2021-05-04 15:27:00 +0200

    interpreter poczatki errorow + opy

--- interpreter/Interpreter.hs
+++ interpreter/Interpreter.hs
@@ -2,6 +2,7 @@ module Interpreter where
 
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Except
 import Lattepp.Abs
 import Data.Map as M
 
@@ -9,10 +10,14 @@ type Var = String
 type Loc = Int
 type Env = M.Map Var Loc
 type LPPStore = M.Map Loc Expr
-type LPPReader = ReaderT Env IO
+type LPPExcept = ExceptT LPPError IO
+type LPPReader = ReaderT Env LPPExcept
 type LPPMonad = StateT LPPStore LPPReader
 
 data Value = VInt Integer | VString String | VBool Bool
+    deriving (Ord, Eq)
+
+data LPPError =  DivisionByZero | ModuloByZero deriving Show
 
 newStore :: LPPStore
 newStore = M.empty 
@@ -34,8 +39,28 @@ execStmt (Ret e) = do
     val <- evalExpr e
     liftIO $ putStrLn ("jol " ++ valueToString val)
 
-evalOp :: (Integer -> Integer -> Integer) -> Integer -> Integer -> LPPMonad Value
-evalOp (+) val1 val2 = return $ VInt (val1 + val2)
+evalAddOp :: AddOp -> Integer -> Integer -> LPPMonad Value
+evalAddOp Plus val1 val2 = return $ VInt (val1 + val2)
+evalAddOp Minus val1 val2 = return $ VInt (val1 - val2)
+
+evalMulOp :: MulOp -> Integer -> Integer -> LPPMonad Value
+evalMulOp Times val1 val2 = return $ VInt (val1 * val2)
+evalMulOp Div val1 val2 = do
+    if val2 == 0 
+        then throwError DivisionByZero 
+        else return $ VInt (val1 `div` val2)
+evalMulOp Mod val1 val2 = do
+    if val2 == 0
+        then throwError ModuloByZero
+        else return $ VInt (val1 `mod` val2)
+
+evalRelOp :: RelOp -> Value -> Value -> LPPMonad Value
+evalRelOp Lt val1 val2 = return $ VBool (val1 < val2)
+evalRelOp Leq val1 val2 = return $ VBool (val1 <= val2)
+evalRelOp Gt val1 val2 = return $ VBool (val1 > val2)
+evalRelOp Geq val1 val2 = return $ VBool (val1 >= val2)
+evalRelOp Eq val1 val2 = return $ VBool (val1 == val2)
+evalRelOp Neq val1 val2 = return $ VBool (val1 /= val2)
 
 evalExpr :: Expr -> LPPMonad Value
 evalExpr (ELitInt x) = return $ VInt x
@@ -45,8 +70,15 @@ evalExpr (EString s) = return $ VString s
 evalExpr (EAdd e1 addOp e2) = do
     VInt val1 <- evalExpr e1
     VInt val2 <- evalExpr e2
-    evalOp (+) val1 val2
-
+    evalAddOp addOp val1 val2
+evalExpr (EMul e1 mulOp e2) = do
+    VInt val1 <- evalExpr e1
+    VInt val2 <- evalExpr e2
+    evalMulOp mulOp val1 val2
+evalExpr (ERel e1 relOp e2) = do
+    val1 <- evalExpr e1
+    val2 <- evalExpr e2
+    evalRelOp relOp val1 val2
 
 execBlock :: Block -> LPPMonad ()
 execBlock (Block []) = return()
@@ -55,6 +87,13 @@ execBlock (Block stmts) = mapM_ execStmt stmts
 execFun :: FunDef -> LPPMonad ()
 execFun (FunDef t ident [] block) = execBlock block
 
-interpret :: Program -> IO ()
+interpret :: Program -> IO (String, Int)
 interpret (Program fundefs) = do
-    void $ runReaderT (runStateT (execFun $ head fundefs) newStore) newEnv
+    let runS = runStateT (execFun $ head fundefs) newStore
+    let runR = runReaderT runS newEnv
+    result <- runExceptT runR
+    case result of
+        Left err -> do
+            return ("Error: " ++ show err, 1)
+        Right msg -> do
+            return ("NoError", 0)
--- interpreter/Main.hs
+++ interpreter/Main.hs
@@ -1,16 +1,25 @@
 module Main where
 
 import System.Environment
+import System.Exit
 
 import Interpreter
 
 import Lattepp.Par
 import Lattepp.ErrM
 
-parseProgram :: String -> IO ()
-parseProgram lppProgram = do
+exitInterpreter :: (String, Int) -> IO ()
+exitInterpreter (msg, 0) = return ()
+exitInterpreter (err, num) = do
+    putStrLn err
+    exitWith $ ExitFailure num
+
+parseAndInterpret :: String -> IO ()
+parseAndInterpret lppProgram = do
     case pProgram (myLexer lppProgram) of
-        Ok prog -> interpret prog
+        Ok prog -> do
+            result <- interpret prog
+            exitInterpreter result
         Bad msg -> putStrLn msg
 
 main :: IO ()
@@ -20,4 +29,4 @@ main = do
         [] -> putStrLn "Podaj plik z programem interpretacji"
         (x:_) -> do
             lppProgram <- readFile x
-            parseProgram lppProgram
+            parseAndInterpret lppProgram

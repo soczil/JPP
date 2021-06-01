f5e39d9aad3d017c7cd2d51ec38c33108958c652
soczil <ksoczewica@gmail.com>
2021-06-01 14:58:40 +0200

    prolog WIP

--- prolog/ks394468.pl
+++ prolog/ks394468.pl
@@ -2,20 +2,45 @@
 
 ensure_loaded(library(lists)).
 
-verify(N, Program) :-
+verify(N, Filename) :-
     write(N),
     write('\n'),
-    readFile(Program).
+    readTerms(Filename, Program),
+    initState(Program, N, InitialState).
 
-readFile(Filename) :- 
+readTerms(Filename, Program) :- 
     set_prolog_flag(fileerrors, off),
     see(Filename),
     !,
     read(vars(Vars)),
     read(arrays(Arrays)),
     read(program(Stmts)),
-    format('JOL: ~n varsy - ~p ~n arraysy - ~p ~n statementsy - ~p ~n', [Vars, Arrays, Stmts]),
+    Program = [Vars, Arrays, Stmts],
     seen.
-readFile(Filename, _) :-
+readTerms(Filename, _) :-
     format('Error: plik ~p nie istnieje ~n', [Filename]).
 
+initState([Vars, Arrays, _], N, InitialState) :-
+    initVars(Vars, VList),
+    initArrays(N, Arrays, AList),
+    makeArray(N, Positions),
+    InitialState = [VList, AList, Positions].
+
+initVars([], []).
+initVars([H | T], VList) :-
+    VList = [(H, 0) | VTail],
+    initVars(T, VTail).
+
+initArrays(_, [], []) :- !. % odciecie!!!!!!!!!!!!!!!!!!!!
+initArrays(N, [H | T], AList) :-
+    makeArray(N, Array),
+    AList = [(H, Array) | ATail],
+    initArrays(N, T, ATail).
+
+makeArray(0, []) :- !. % odciecie!!!!!!!!!!!!!!!!!!!!!
+makeArray(N, Array) :-
+    M is N - 1,
+    Array = [0 | T],
+    makeArray(M, T).
+
+% execStmt(assign(X, Expr), StateIn, Pid, StateOut) :-

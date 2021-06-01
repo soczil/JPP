8275cc6aaa478685e0fae85ff13c81df4b4cfd46
soczil <ksoczewica@gmail.com>
2021-06-01 15:54:51 +0200

    prolog WIP

--- prolog/ks394468.pl
+++ prolog/ks394468.pl
@@ -44,3 +44,47 @@ makeArray(N, Array) :-
     makeArray(M, T).
 
 % execStmt(assign(X, Expr), StateIn, Pid, StateOut) :-
+
+evalExpr(X, _, X) :-
+    number(X),
+    !.
+evalExpr(Var, [Vars, _, _], Res) :-
+    member((Var, _), Vars),
+    getVarValue(Var, Vars, Res),
+    !.
+evalExpr(E1 + E2, State, Res) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res is Res1 + Res2.
+evalExpr(E1 - E2, State, Res) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res is Res1 - Res2.
+evalExpr(E1 * E2, State, Res) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res is Res1 * Res2.
+evalExpr(E1 / E2, State, Res) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res is Res1 / Res2.
+
+evalBoolExpr(E1 < E2, State) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res1 < Res2.
+evalBoolExpr(E1 = E2, State) :-
+    evalExpr(E1, State, Res1),
+    evalExpr(E2, State, Res2),
+    Res1 =:= Res2.
+% evalBoolExpr(E1 <> E2, State) :-
+%     evalExpr(E1, State, Res1),
+%     evalExpr(E2, State, Res2),
+%     Res1 =\= Res2.
+
+getVarValue(_, [(_, Val)], Val) :- !.
+getVarValue(Var, [(Id, Val) | T], Res) :-
+    (   Var \= Id
+    ->  getVarValue(Var, T, Res)
+    ;   Res = Val
+    ).
